820ac8ecbb622af68fc926c4f1dd5921e0c6968a
Lovelesh Kumar <lovelesh.kumar@nagarro.com>
2025-01-09 21:09:17 +0530

    Insurance Details - Import MyPoliciesModule in AppModule, Remove default html from app component, Add routing

--- projects/insurance-details/src/app/app-routing.module.ts
+++ projects/insurance-details/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyPoliciesComponent } from './my-policies/my-policies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/my-policies', pathMatch: 'full'},
+  { path: 'my-policies', component: MyPoliciesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/insurance-details/src/app/app.module.ts
+++ projects/insurance-details/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyPoliciesModule } from './my-policies/my-policies.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MyPoliciesModule,
     AppRoutingModule
   ],
   providers: [],
