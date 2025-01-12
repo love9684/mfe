275a1125ad24b1cdc198b7fd26a7fac64eb91210
Lovelesh Kumar <lovelesh.kumar@nagarro.com>
2025-01-12 23:56:55 +0530

    Use deploy url of insurance-detail app in webpack prod

--- projects/container-app/webpack.prod.config.js
+++ projects/container-app/webpack.prod.config.js
@@ -1,1 +1,58 @@
-module.exports = require('./webpack.config');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "containerApp",
+    publicPath: "auto",
+    scriptType: "text/javascript"
+  },
+  optimization: {
+    runtimeChunk: false
+  },   
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+        // No need for now
+        // library: { type: "module" },
+
+        // For remotes (please adjust)
+        name: "containerApp",
+        // filename: "remoteEntry.js",
+        // exposes: {
+        //     './Component': './projects/container-app/src/app/app.component.ts',
+        // },        
+        
+        // For hosts (please adjust)
+        remotes: {
+            "insuranceDetails": "insuranceDetails@https://nagp-insurance-details.netlify.app/remoteEntry.js",
+            "premiumPayment": "premiumPayment@https://ephemeral-griffin-1609ac.netlify.app:4202/remoteEntry.js",
+        },
+
+        shared: share({
+          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
+          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+
+          ...sharedMappings.getDescriptors()
+        })
+        
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
